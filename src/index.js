3a975f582d95d63e61f7117ee45ab144ed3f5cd9
Jakedalus <jacquedalus@gmail.com>
2018-11-27 14:42:17 -0500

    Add code splitting with System.import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-// const sum = require('./sum');  // CommonJS modules
-import sum from './sum';  // ES2015 modules
-import './image-viewer';
 
-console.log(sum(7, 5));
+const button = document.createElement('button');
+button.innerText = 'Click Me';
+button.onclick = () => {
+    System.import('./image-viewer').then((module) => {
+        module.default();
+    });
+};
+
+document.body.appendChild(button);
+
+
+
+////// Old Code
+// // const sum = require('./sum');  // CommonJS modules
+// import sum from './sum';  // ES2015 modules
+// import './image-viewer';
+
+// console.log(sum(7, 5));
